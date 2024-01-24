80b542ce3223819f9c3228bfa775c1383f1030c5
MahdiHasan943 <mahdifreelancer41@gmail.com>
2024-01-24 12:07:16 +0600

    added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import Link from "next/link";
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
-
+import { useEffect, useRef ,useState } from "react";
+import emailjs from "@emailjs/browser";
+import toast, { Toaster } from 'react-hot-toast';
 export default function Home() {
+  const form = useRef();
+
     const { systemTheme, theme, setTheme } = useTheme();
     const [mounted, setMounted] = useState(false);
     useEffect(() => {
@@ -10,7 +13,33 @@ export default function Home() {
     }, []);
   
     if (!mounted) return null;
-    const currentTheme = theme === "system" ? systemTheme : theme;
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "",
+        "",
+        form.current,
+        ""
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+          form.current.reset();
+          toast.success('Successfully submitted form');
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+
+
   return (
     <>
       <div className="px-4 sm:16 py-4">
@@ -66,7 +95,55 @@ export default function Home() {
         
         <div className="max-w-[1500px] py-10 mx-auto">
         <img className="mx-auto py-7" src="/Frame 20.png" alt="" />
-       </div>
+        </div>
+
+
+        <div className="max-w-[1500px] py-10 mx-auto">
+
+        <form className="max-w-[700px] mx-auto" ref={form} onSubmit={sendEmail} action="">
+              <div className="py-20">
+                <input
+                  type="text"
+                  className="placeholder:text-[#010101] border border-[black] text-[#010101] placeholder:text-[22px] py-8 px-6 rounded-xl w-full bg-white"
+                  placeholder="First & Last Name  *"
+                  name="user_name"
+                />
+                <input
+                  type="email"
+                  className="placeholder:text-[#010101] border border-[black] placeholder:text-[22px] text-[#010101] py-8 px-6 rounded-xl my-10 w-full bg-white"
+                  placeholder="Email Address *"
+                  name="user_email"
+              />
+                <input
+                  type="text"
+                  className="placeholder:text-[#010101] border border-[black] text-[#010101] placeholder:text-[22px] py-8 mb-10 px-6 rounded-xl w-full bg-white"
+                  placeholder="Phone Number"
+                  name="phone"
+                />
+                <textarea
+                  className="w-full px-6 border border-[black] placeholder:text-[#010101] text-[#010101] rounded-xl placeholder:text-[22px] py-6 bg-white"
+                  placeholder="Enter your condolence message here"
+                  name="message"
+                  id=""
+                  cols="30"
+                  rows="7"
+                ></textarea>
+                <div className="text-center pt-10">
+                  <input
+                    type="submit"
+                    value="Send it"
+                    className="text-white rounded-xl text-[20px] font-normal leading-[30px] bg-[#843E71] py-4  sm:py-10 px-10 w-[50%] "
+                  />
+                </div>
+              </div>
+            </form>
+
+
+        </div>
+        
+        <Toaster />
+        
+
 
 </div>
 
